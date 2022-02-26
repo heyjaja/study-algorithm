5ee33015dc171fb1147d210d76d462e1ef60c791
heyjaja <imjaja09@gmail.com>
2022-02-26 17:37:44 +0900

    .DS_Store removed

---
+++ Baekjoon/src/bj/array/Array2562.java
@@ -0,0 +1,25 @@
+package bj.array;
+
+import java.util.Scanner;
+
+public class Array2562 {
+
+  public static void main(String[] args) {
+    //최대값을 찾고 몇 번째 수인지 구하기
+    //9개의 서로 다른 자연수
+    Scanner sc = new Scanner(System.in);
+    int arr[] = new int[9];
+    int max=0;
+    int no=-1;
+    for(int i=0; i<9; i++) {
+      arr[i] = sc.nextInt();
+      if (arr[i]>max) {
+        max=arr[i];
+        no=i+1;
+      }
+    }
+    System.out.println(max);
+    System.out.println(no);
+  }
+
+}
---
+++ Baekjoon/src/bj/array/Array2577.java
@@ -0,0 +1,10 @@
+package bj.array;
+
+public class Array2577 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
