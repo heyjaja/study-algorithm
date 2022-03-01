c4985ca2a49cc01c841cef03b1915bdeb60222d7
heyjaja <imjaja09@gmail.com>
2022-03-01 18:25:57 +0900

    2022/03/01

---
+++ Baekjoon/src/bj/array/Array4344.java
@@ -0,0 +1,47 @@
+package bj.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Array4344 {
+
+  public static void main(String[] args) throws IOException {
+    // 첫째줄 테스트 케이스 C
+    // 둘째줄부터 한줄로 학생 수 N, 점수 N개
+    // 평균이 넘는 학생의 비율을 소수점 3자리까지 반올림하여 출력
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int c = Integer.parseInt(br.readLine());
+    
+    while(c>0) {
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+      
+      int arr[] = new int[Integer.parseInt(st.nextToken())];
+      
+      double sum = 0;
+      
+      for(int i=0; i<arr.length; i++) {
+        arr[i] = Integer.parseInt(st.nextToken());
+        sum+=arr[i];
+      }
+      
+      double avg = sum/arr.length;
+      double count = 0;
+      
+      for(int i=0; i<arr.length; i++) {
+        if(arr[i]>avg) {
+          count++;
+        }
+      }
+      
+      System.out.printf("%.3f%%\n", (count/arr.length*100));
+      
+      c--;
+      
+    }
+    
+    
+  }
+
+}
---
+++ Baekjoon/src/bj/array/Array8958.java
@@ -0,0 +1,38 @@
+package bj.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Array8958 {
+
+  public static void main(String[] args) throws IOException {
+    // OX 퀴즈 정답 O, 오답 X
+    // 정답당 1점씩 연속 정답 시 1+2+3+...
+    // 첫째줄에 테스트케이스 n
+    // 둘째줄부터 OX 퀴즈 정답 문자열 한줄씩 "OOXXOXOXOOX"
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    String arr[] = new String[n];
+    
+    
+    for(int i=0; i<n; i++) {
+      int score=0;
+      int sum=0;
+      arr[i] = br.readLine();
+      for(char c : arr[i].toCharArray()) {
+        if(c=='O') {
+          score += 1;
+          sum += score;
+        } else {
+          score = 0;
+        }
+      }
+      System.out.println(sum);
+    }
+    
+    
+  }
+
+}
