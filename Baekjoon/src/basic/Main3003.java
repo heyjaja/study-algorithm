ee4fa8d24fdedb12cf700f6bdaa3eb13324adb34
heyjaja <imjaja09@gmail.com>
2022-09-08 02:32:39 +0900

    2022/09/08

---
+++ Baekjoon/src/basic/Main3003.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main3003 {
+  
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    
+    int[] arr = {1, 1, 2, 2, 2, 8};
+    
+    for(int i=0; i<arr.length; i++) {
+      int n = Integer.parseInt(st.nextToken());
+      
+      arr[i] = arr[i] - n;
+      
+      System.out.print(arr[i] + " ");
+    }
+    
+    
+  }
+
+}
---
+++ Baekjoon/src/greedy/Main2839.java
@@ -0,0 +1,24 @@
+package greedy;
+
+import java.util.Scanner;
+
+public class Main2839 {
+
+  public static void main(String[] args) {
+    // Nkg
+    // 3, 5 kg
+    // 더 적은 개수 봉지
+    // 첫째 줄에 N이 주어진다. (3 ≤ N ≤ 5000)
+    // 봉지의 최소 개수 or -1
+    
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    
+    int count = -1;
+    
+
+  }
+  
+ 
+
+}
