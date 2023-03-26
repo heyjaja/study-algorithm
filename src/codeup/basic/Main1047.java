fca9b0669a8e0984ec768af5567881c44b9b57d6
heyjaja <imjaja09@gmail.com>
2023-03-26 22:31:25 +0900

    made intelliJ project

--- src/codeup/basic/Main1047.java
+++ src/codeup/basic/Main1047.java
@@ -3,7 +3,6 @@ package basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main1047 {
   public static void main(String[] args) throws IOException {
