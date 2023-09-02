3c58338719cec5fb722712c942155c16aa3699f0
heyjaja <imjaja09@gmail.com>
2023-09-02 22:15:57 +0900

    4-1 학급회장(HashMap), 4-2 아나그램(HashMap)

---
+++ src/basic/hashmap/Anagram.java
@@ -0,0 +1,36 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Anagram {
+    public String solution(String a, String b) {
+        String answer = "YES";
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : a.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : b.toCharArray()) {
+            if(!map.containsKey(c) || map.get(c) == 0) {
+                return "NO";
+            }
+            map.put(c, map.get(c) - 1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 아나그램 알파벳의 개수가 일치하여 재배치하면 서로 같은 단어
+        // 길이가 같은 두 문자열이 서로 아나그램인지 판별
+        Anagram main = new Anagram();
+
+        Scanner sc = new Scanner(System.in);
+        String input1 = sc.next();
+        String input2 = sc.next();
+
+        System.out.println(main.solution(input1, input2));
+    }
+}
---
+++ src/basic/hashmap/ClassPresident.java
@@ -0,0 +1,38 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ClassPresident {
+    public char solution(int n, String str) {
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int max = 0;
+        for(char key : map.keySet()) {
+            if(map.get(key) > max) {
+                max = map.get(key);
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 학급 회장 후보 A,B,C,D,E
+        // 학생 수 N, 투표 결과 기호 문자열
+        // 가장 많은 표를 얻은 후보의 기호 출력
+        ClassPresident main = new ClassPresident();
+
+        Scanner sc = new Scanner(System.in);
+        int input1 = sc.nextInt();
+        String input2 = sc.next();
+
+        System.out.println(main.solution(input1, input2));
+    }
+}
