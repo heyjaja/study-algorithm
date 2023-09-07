97b90e2f10ab436650f77f0d7ed877ebf5cc04e5
heyjaja <imjaja09@gmail.com>
2023-09-07 22:20:25 +0900

    5-4 후위식 연산(stack)

---
+++ src/basic/collections/Postfix.java
@@ -0,0 +1,44 @@
+package collections;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Postfix {
+    public int solution(String s) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if(Character.isDigit(c)) {
+                stack.push(c-48);
+                continue;
+            }
+            int rt = stack.pop();
+            int lt = stack.pop();
+
+            if(c == '+') {
+                stack.push(lt + rt);
+            } else if(c == '-') {
+                stack.push(lt - rt);
+            } else if(c == '*') {
+                stack.push(lt * rt);
+            } else if(c == '/') {
+                stack.push(lt / rt);
+            }
+        }
+
+        answer = stack.pop();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 후위식 연산
+        Postfix main = new Postfix();
+
+        Scanner sc = new Scanner(System.in);
+        String input1 = sc.next();
+
+        System.out.println(main.solution(input1));
+    }
+}
