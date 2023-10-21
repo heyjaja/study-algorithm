565e1bd5907285608455019901094d0c8212cda3
heyjaja <imjaja09@gmail.com>
2023-10-21 23:13:05 +0900

    8-6 순열 구하기(DFS)

--- src/basic/dfs/Permutation.java
+++ src/basic/dfs/Permutation.java
@@ -4,7 +4,29 @@ import java.util.Scanner;
 
 public class Permutation {
     static int[] pm;
-    public void DFS(int n, int m, int level) {
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        // 순열 구하기
+        // N개의 자연수로 M 길이의 순열 구하기(중복x)
+        Permutation main = new Permutation();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+        pm = new int[m];
+        check = new boolean[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        main.DFS(0, n, m, arr);
+
+    }
+
+    private void DFS(int level, int n, int m, int[] arr) {
         if(level == m) {
             for(int i : pm) {
                 System.out.print(i + " ");
@@ -12,25 +34,15 @@ public class Permutation {
             System.out.println();
             return;
         }
-        for(int i=1; i<=n; i++) {
-            pm[level] = i;
-            DFS(n, m, level+1);
+        for(int i=0; i<n; i++) {
+            // arr[i] 사용 여부 체크
+            if(check[i]) {
+                continue;
+            }
+            pm[level] = arr[i];
+            check[i] = true;
+            DFS(level + 1, n, m, arr);
+            check[i] = false;
         }
-    }
-
-    public static void main(String[] args) {
-        // 중복순열 구하기(DFS)
-        // 1~N까지 번호가 적인 구슬
-        // M번을 뽑아 일렬로 나열하는 방법(중복 가능)
-        // 오름차순 출력
-        Permutation main = new Permutation();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        pm = new int[m];
-
-        main.DFS(n, m, 0);
-
-
     }
 }
---
+++ src/basic/dfs/PermutationWithRepetition.java
@@ -0,0 +1,36 @@
+package dfs;
+
+import java.util.Scanner;
+
+public class PermutationWithRepetition {
+    static int[] pm;
+    public void DFS(int n, int m, int level) {
+        if(level == m) {
+            for(int i : pm) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i=1; i<=n; i++) {
+            pm[level] = i;
+            DFS(n, m, level+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 중복순열 구하기(DFS)
+        // 1~N까지 번호가 적인 구슬
+        // M번을 뽑아 일렬로 나열하는 방법(중복 가능)
+        // 오름차순 출력
+        PermutationWithRepetition main = new PermutationWithRepetition();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        pm = new int[m];
+
+        main.DFS(n, m, 0);
+
+
+    }
+}
