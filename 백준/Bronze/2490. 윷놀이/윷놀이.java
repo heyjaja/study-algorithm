685fd6b1b1cfd8bd097acb053f13a4545b8c7063
jaja <89507047+heyjaja@users.noreply.github.com>
2025-03-10 08:52:03 +0900

    [Bronze III] Title: 윷놀이, Time: 176 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -1,22 +1,17 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         // 배 0, 등 1
         // 3줄 ABCDE(도개걸윷모)
         Scanner sc = new Scanner(System.in);
-        String s = "EABCD";
-        char[] charArr = new char[3];
+        String s = "DCBAE";
         for(int i=0; i<3; i++) {
-            String a = sc.nextLine();
             int count = 0;
-            for(String c : a.split(" ")) {
-                if("0".equals(c)) count++;
+            for (int j = 0; j < 4; j++) {
+                count += sc.nextInt();
             }
-            charArr[i] = s.charAt(count);
-        }
-
-        for (char c : charArr) {
-            System.out.println(c + " ");
+            System.out.println(s.charAt(count));
         }
 
     }
