b2a7490b027d760acc51abb15747b12a62338b0d
jaja <89507047+heyjaja@users.noreply.github.com>
2025-03-10 14:11:00 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 172 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -4,53 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Policy y = new Policy("Y", 30, 10);
-        Policy m = new Policy("M", 60, 15);
-
         int n = sc.nextInt();
-        int[] arr = new int[n];
+
+        int y = 0;
+        int m = 0;
         for(int i=0; i<n; i++) {
-            arr[i] = sc.nextInt();
+            int a = sc.nextInt();
+            y += (a/30 + 1) * 10;
+            m += (a/60 + 1) * 15;
         }
 
-        int yResult = y.calculatePrices(n, arr);
-        int mResult = m.calculatePrices(n, arr);
-
-        if(yResult < mResult) {
-            System.out.println(y.name + " " + yResult);
-        } else if(mResult < yResult) {
-            System.out.println(m.name + " " + mResult);
+        StringBuilder sb = new StringBuilder();
+        if(y < m) {
+            sb.append("Y ").append(y);
+        } else if(m < y) {
+            sb.append("M ").append(m);
         } else {
-            System.out.println(y.name + " " + m.name + " " + yResult);
-        }
-    }
-}
-
-class Policy {
-    String name;
-    int time;
-    int rate;
-
-    public Policy(String name, int time, int rate) {
-        this.name = name;
-        this.time = time;
-        this.rate = rate;
-    }
-
-    public int calculatePrices(int n, int[] seconds) {
-        int sum = 0;
-        for(int i=0; i<n; i++) {
-            sum += calculatePrice(seconds[i]);
+            sb.append("Y M ").append(y);
         }
 
-        return sum;
-    }
-
-    private int calculatePrice(int seconds) {
-        if(seconds < time) {
-            return rate;
-        }
-
-        return (seconds/time + 1) * rate;
+        System.out.println(sb);
     }
 }
