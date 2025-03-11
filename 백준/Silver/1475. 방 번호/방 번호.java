93fce3692c4900d497dc419c6778a1f6824c4f33
jaja <89507047+heyjaja@users.noreply.github.com>
2025-03-11 10:29:29 +0900

    [Silver V] Title: 방 번호, Time: 172 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -5,24 +5,28 @@ public class Main {
         // 다솜이의 방 번호 N <= 1000000
         // 0~9 숫자 세트(6, 9는 서로 대체 가능) 몇 개 필요
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        String s = sc.next();
         int[] arr = new int[10];
 
-        while(n > 0) {
-            int x = n%10;
-            if(x == 6 || x == 9) {
+        for(char c : s.toCharArray()) {
+            int index = c - '0';
+            if(index == 6 || index == 9) {
                 if(arr[6] < arr[9]) {
                     arr[6]++;
                 } else {
                     arr[9]++;
                 }
             } else {
-                arr[x]++;
+                arr[index]++;
             }
-            n/=10;
         }
 
-        System.out.println(Arrays.stream(arr).max().getAsInt());
+        int max = 0;
+        for (int i : arr) {
+            if(i>max) max = i;
+        }
+
+        System.out.println(max);
     }
 
 }
