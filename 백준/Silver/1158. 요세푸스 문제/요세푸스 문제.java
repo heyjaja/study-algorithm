401574c9d3dbb128d5cf9bdc01932c57e3372631
jaja <89507047+heyjaja@users.noreply.github.com>
2025-03-13 11:28:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 204 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        IntQueue queue = new IntQueue(n);
+        for(int i=0; i<n; i++) {
+            queue.offer(i+1);
+        }
+
+        sb.append('<');
+        while(!queue.isEmpty()) {
+            int x = 0;
+            for(int i=1; i<=k; i++) {
+                int a = queue.poll();
+                if(i == k) {
+                    x = a;
+                    break;
+                }
+                queue.offer(a);
+            }
+            sb.append(x);
+            if(!queue.isEmpty()) sb.append(", ");
+        }
+        sb.append('>');
+
+        System.out.println(sb);
+
+    }
+}
+
+class IntQueue {
+    int[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public IntQueue(int max) {
+        size = front = rear = 0;
+        this.max = max;
+        data = new int[max];
+    }
+
+    public int offer(int n) {
+        data[rear++] = n;
+        size++;
+
+        if(rear == max) {
+            rear = 0;
+        }
+
+        return n;
+    }
+
+    public int poll() {
+        int n = data[front++];
+        size--;
+        if(front == max) {
+            front = 0;
+        }
+
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
