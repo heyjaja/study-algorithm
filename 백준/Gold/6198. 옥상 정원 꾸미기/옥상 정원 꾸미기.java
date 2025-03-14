3d02e2baff941d8c9b846af4beebe845fb586a39
tomato-jaeun <jaja@etomato.com>
2025-03-14 11:34:55 +0900

    백준: 스택 문제 주석 추가

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -13,14 +13,14 @@ public class Main {
 
         long count = 0;
         Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i<n; i++) {
-            int building = buildings[i];
-            while(!stack.isEmpty() && stack.peek() <= building) {
-                stack.pop();
+        for(int i = 0; i<n; i++) { // 왼쪽 -> 오른쪽 탐색
+            int building = buildings[i]; // 현재 빌딩
+            while(!stack.isEmpty() && stack.peek() <= building) { // 현재 빌딩보다 낮은 빌딩(현재 빌딩을 볼 수 없는 빌딩) pop
+                stack.pop(); // 현재 빌딩보다 낮은 빌딩은 이후 빌딩을 볼 수 없음(제거)
             }
 
-            count += stack.size();
-            stack.push(building);
+            count += stack.size(); // 스택에 남아있는 빌딩은 현재 빌딩을 볼 수 있는 빌딩의 수
+            stack.push(building); // 현재 빌딩 스택에 추가
         }
 
         System.out.println(count);
