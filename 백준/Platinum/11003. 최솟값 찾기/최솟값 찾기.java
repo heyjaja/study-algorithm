69da66d0130e27f94addd0ab9d53bab7328454e0
tomato-jaeun <jaja@etomato.com>
2025-03-17 14:44:06 +0900

    백준: 최솟값 찾기 설명 추가

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -14,12 +14,13 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
             Number num = new Number(i+1, Integer.parseInt(st.nextToken()));
-            if(!dq.isEmpty() && dq.peekFirst().index < num.index - l + 1) {
+            if(!dq.isEmpty() && dq.peekFirst().index < num.index - l + 1) { // 범위를 벗어나는 요소 삭제
                 dq.pollFirst();
             }
 
-            while(!dq.isEmpty() && dq.peekLast().value > num.value) dq.pollLast();
-            dq.addLast(num);
+            // 현재 값이 더 작다면 덱에 있는 현재 값 보다 큰 값은 미래에 최솟값(과거는 이미 처리됨)이 될 수 없음
+            while(!dq.isEmpty() && dq.peekLast().value > num.value) dq.pollLast(); // 오름차순을 유지하기 위해 제거
+            dq.addLast(num); // 새로운 값을 덱의 마지막에 추가하여 최솟값/오래된 값을 맨 앞으로 유지
             sb.append(dq.peekFirst().value).append(' ');
         }
         System.out.println(sb);
