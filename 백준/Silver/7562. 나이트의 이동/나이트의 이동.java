b6eb0eab48f44648b70e836f31a881ae0ec302e2
jaja <89507047+heyjaja@users.noreply.github.com>
2025-03-22 21:46:06 +0900

    [Silver I] Title: 나이트의 이동, Time: 268 ms, Memory: 70624 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -5,56 +5,53 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine()); // 테스트케이스 개수
-        int[] dx = {2, 1, -1, -2, -2, -1, 1, 2};
-        int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
 
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         for(int t=0; t<T; t++) { // 테스트 케이스
             int I = Integer.parseInt(br.readLine()); // 체스판 한 변의 길이
-            int[][] board = new int[I][I];
-            int[][] dis = new int[I][I];
-            Queue<Pair> q = new LinkedList<>();
-            boolean flag = false;
-            for(int i=1; i<=2; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
 
-                board[x][y] = i;
-                if(dis[x][y] > 0) {
-                    flag = true;
-                    sb.append("0\n");
-                    break;
-                }
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-                if(i==1) {
-                    dis[x][y] = 1;
-                    q.offer(new Pair(x, y));
-                }
+            st = new StringTokenizer(br.readLine());
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
 
-            }
-
-            while(!flag && !q.isEmpty()) {
-                Pair cur = q.poll();
-                for(int i=0; i<8; i++) {
-                    int nx = cur.x + dx[i];
-                    int ny = cur.y + dy[i];
-                    if(nx < 0 || nx >= I || ny < 0 || ny >= I) continue;
-                    if(dis[nx][ny] > 0) continue;
-                    if(board[nx][ny] == 2) {
-                        sb.append(dis[cur.x][cur.y]).append('\n');
-                        flag = true;
-                        break;
-                    }
-                    dis[nx][ny] = dis[cur.x][cur.y] + 1;
-                    q.offer(new Pair(nx, ny));
-                }
-            }
-
+            sb.append(bfs(I, x, y, endX, endY)).append('\n');
         }
         System.out.println(sb);
 
+    }
+
+    private static int bfs(int I, int x, int y, int endX, int endY) {
+        if(x == endX && y == endY) return 0;
+
+        int[][] dis = new int[I][I];
+        int[] dx = {2, 1, -1, -2, -2, -1, 1, 2};
+        int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+
+        Queue<Pair> q = new LinkedList<>();
+        dis[x][y] = 1;
+        q.offer(new Pair(x,y));
+
+        while(dis[endX][endY] == 0) {
+            Pair cur = q.poll();
+            for(int i=0; i<8; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if(nx < 0 || nx >= I || ny < 0 || ny >= I) continue;
+                if(dis[nx][ny] > 0) continue;
+                if(nx == endX && ny == endY) {
+                    return dis[cur.x][cur.y];
+                }
+                dis[nx][ny] = dis[cur.x][cur.y] + 1;
+                q.offer(new Pair(nx, ny));
+            }
+        }
+
+        return dis[endX][endY];
     }
 }
 
