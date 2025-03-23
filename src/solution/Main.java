51d89b1775e783962740775c95a69869b803ff3e
heyjaja <imjaja09@gmail.com>
2025-03-23 17:59:13 +0900

    백준: Main class git ignore에서 삭제

---
+++ src/solution/Main.java
@@ -0,0 +1,22 @@
+package solution;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //https://www.acmicpc.net/workbook/view/7313
+
+    }
+}
+
+class Pair {
+    int x;
+    int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
