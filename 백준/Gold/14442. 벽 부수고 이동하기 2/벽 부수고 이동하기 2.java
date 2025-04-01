94e81e6cfacab15128938320032e722199436e8b
jaja <89507047+heyjaja@users.noreply.github.com>
2025-04-01 17:14:25 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1912 ms, Memory: 410888 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N][M];
+        for(int i=0; i<N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        int[][][] vis = new int[N][M][K+1];
+        Queue<Point> q = new LinkedList<>();
+        vis[0][0][0] = 1;
+        q.offer(new Point(0, 0, 0));
+
+        while(!q.isEmpty()) {
+            Point cur = q.poll();
+            for(int i=0; i<4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if(vis[nx][ny][cur.broken] == 0 && map[nx][ny] == 0) {
+                    vis[nx][ny][cur.broken] = vis[cur.x][cur.y][cur.broken] + 1;
+                    q.offer(new Point(nx, ny, cur.broken));
+                } else if(map[nx][ny] == 1 && cur.broken < K && vis[nx][ny][cur.broken+1] == 0) {
+                    vis[nx][ny][cur.broken + 1] = vis[cur.x][cur.y][cur.broken] + 1;
+                    q.offer(new Point(nx, ny, cur.broken+1));
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i<=K; i++) {
+            if(vis[N-1][M-1][i] == 0) continue;
+            min = Math.min(min, vis[N-1][M-1][i]);
+        }
+
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+
+    }
+}
+
+class Point {
+    int x;
+    int y;
+    int broken;
+
+    public Point(int x, int y, int broken) {
+        this.x = x;
+        this.y = y;
+        this.broken = broken;
+    }
+}
