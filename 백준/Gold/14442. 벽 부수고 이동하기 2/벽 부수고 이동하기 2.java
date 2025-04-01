2d8d64f0c8203f0fa18287884f688f16a2bd4a4e
jaja <89507047+heyjaja@users.noreply.github.com>
2025-04-01 17:18:19 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1612 ms, Memory: 383540 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -20,7 +20,7 @@ public class Main {
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
 
-        int[][][] vis = new int[N][M][K+1];
+        int[][][] vis = new int[K+1][N][M];
         Queue<Point> q = new LinkedList<>();
         vis[0][0][0] = 1;
         q.offer(new Point(0, 0, 0));
@@ -32,11 +32,11 @@ public class Main {
                 int ny = cur.y + dy[i];
 
                 if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                if(vis[nx][ny][cur.broken] == 0 && map[nx][ny] == 0) {
-                    vis[nx][ny][cur.broken] = vis[cur.x][cur.y][cur.broken] + 1;
+                if(vis[cur.broken][nx][ny] == 0 && map[nx][ny] == 0) {
+                    vis[cur.broken][nx][ny] = vis[cur.broken][cur.x][cur.y] + 1;
                     q.offer(new Point(nx, ny, cur.broken));
-                } else if(map[nx][ny] == 1 && cur.broken < K && vis[nx][ny][cur.broken+1] == 0) {
-                    vis[nx][ny][cur.broken + 1] = vis[cur.x][cur.y][cur.broken] + 1;
+                } else if(map[nx][ny] == 1 && cur.broken < K && vis[cur.broken+1][nx][ny] == 0) {
+                    vis[cur.broken + 1][nx][ny] = vis[cur.broken][cur.x][cur.y] + 1;
                     q.offer(new Point(nx, ny, cur.broken+1));
                 }
             }
@@ -44,8 +44,8 @@ public class Main {
 
         int min = Integer.MAX_VALUE;
         for (int i = 0; i<=K; i++) {
-            if(vis[N-1][M-1][i] == 0) continue;
-            min = Math.min(min, vis[N-1][M-1][i]);
+            if(vis[i][N-1][M-1] == 0) continue;
+            min = Math.min(min, vis[i][N-1][M-1]);
         }
 
         System.out.println(min == Integer.MAX_VALUE ? -1 : min);
