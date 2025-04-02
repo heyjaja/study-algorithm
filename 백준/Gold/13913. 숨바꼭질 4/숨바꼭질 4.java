1dc81527e314174eded821be491141310564557f
jaja <89507047+heyjaja@users.noreply.github.com>
2025-04-02 11:49:38 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 264 ms, Memory: 32316 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -9,53 +9,42 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        if(N == K) {
-            System.out.println(0);
-            System.out.println(N);
-            return;
-        } else if(N > K) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(N-K).append('\n');
-            for (int i = N; i >= K; i--) {
-                sb.append(i).append(' ');
-            }
-            System.out.println(sb);
-            return;
+
+        int[] prev = new int[100001];
+        Arrays.fill(prev, -1); // 0부터 시작할 수 있으니 -1로 초기화
+        Queue<Integer> q = new LinkedList<>();
+        prev[N] = 1; // 이전 경로를 저장
+        q.offer(N);
+
+        while(prev[K] == -1 && !q.isEmpty()) {
+            Integer cur = q.poll();
+            check(prev, q, cur, cur-1);
+            check(prev, q, cur, cur+1);
+            check(prev, q, cur, cur*2);
+        }
+
+        Stack<Integer> s = new Stack<>();
+
+        s.push(K);
+
+        int index = K;
+        while(index != N) { // 목표 위치부터 처음 위치가 나올 때까지 반복하여 stack에 push -> 역추적
+            s.push(prev[index]);
+            index = prev[index];
         }
 
         StringBuilder sb = new StringBuilder();
-        int[] dis = new int[100001];
-        Queue<Point> q = new LinkedList<>();
-        dis[N] = 1;
-        q.offer(new Point(N, new StringBuilder()));
-
-        while(dis[K] == 0 && !q.isEmpty()) {
-            Point cur = q.poll();
-            sb = cur.path;
-            check(dis, q, cur, cur.x-1);
-            check(dis, q, cur, cur.x+1);
-            check(dis, q, cur, cur.x*2);
+        sb.append(s.size() - 1).append('\n'); // 스택의 크기가 경로의 길이
+        while(!s.isEmpty()) {
+            sb.append(s.pop()).append(' ');
         }
-
-        System.out.println(dis[K] - 1);
-        System.out.println(sb.append(K));
-
+        System.out.println(sb);
     }
 
-    private static void check(int[] dis, Queue<Point> q, Point cur, int nx) {
-        if(nx < 0 || nx >= dis.length) return;
-        if(dis[nx] != 0 || dis[nx] >= dis[cur.x]) return;
-        dis[nx] = dis[cur.x] + 1;
-        q.offer(new Point(nx, new StringBuilder(cur.path)));
-    }
-}
-
-class Point {
-    int x;
-    StringBuilder path;
-
-    public Point(int x, StringBuilder past) {
-        this.x = x;
-        this.path = past.append(x).append(" ");
+    private static void check(int[] prev, Queue<Integer> q, Integer cur, int nx) {
+        if(nx < 0 || nx >= prev.length) return;
+        if(prev[nx] > -1) return; // prev[nx]가 -1 이상이면 이미 방문한 위치
+        prev[nx] = cur; // 다음 위치 index에 현재 위치 저장, 다음 위치 전 방문한 위치가 현재 위치가 됨
+        q.offer(nx);
     }
 }
