cfb56370d8445202de448d21210249e26c3dc103
tomato-jaeun <jaja@etomato.com>
2025-04-04 17:01:05 +0900

    백준: 9466 텀 프로젝트 설명 추가

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -26,35 +26,34 @@ public class Main {
         boolean[] inTeam = new boolean[n+1];
         int count = 0;
         for (int i = 1; i < n + 1; i++) {
-            if(vis[i] != 0) continue;
+            if(vis[i] != 0) continue; // 이미 방문한 학생은 건너뛰기
             int cur = i;
 
-            // 이번 방문에서 지나간 학생에 도달한 경우
             while(vis[cur] == 0) {
-                vis[cur] = 1;
-                cur = students[cur];
+                vis[cur] = 1; // 현재 경로에서 방문 중 표시
+                cur = students[cur]; // 다음 학생으로 이동
             }
 
-            if(vis[cur] == 1) {
-                int start = cur;
-                inTeam[cur] = true;
-                cur = students[cur];
+            if(vis[cur] == 1) { // 사이클 발견 -> 현재 경로에서 방문한 노드를 다시 방문
+                int start = cur; // 현재 탐색 학생 저장
+                inTeam[cur] = true; // 현재 학생은 사이클 내에 있음 -> 팀이 됨
+                cur = students[cur]; // 다음 학생으로
 
-                while(cur  != start) {
+                while(cur  != start) { // 사이클을 돌면서 팀으로 만들기
                     inTeam[cur] = true;
                     cur = students[cur];
                 }
             }
 
-            cur = i;
-            while(vis[cur] == 1) {
+            cur = i; // 탐색 시작 학생으로 복귀
+            while(vis[cur] == 1) { // 이번 경로에서 방문한 학생은 모두 방문 완료로 수정 -> 팀이 안된 학생 포함
                 vis[cur] = 2;
                 cur = students[cur];
             }
         }
 
         for (int i = 1; i <= n; i++) {
-            if(!inTeam[i]) count++;
+            if(!inTeam[i]) count++; // 사이클에 포함되지 않은 학생들은 팀이 안됨
         }
 
         return count;
