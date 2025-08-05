9df74a37ec311592d6ca5d2adc6e03e1af47baa7
jaja <89507047+heyjaja@users.noreply.github.com>
2025-08-05 19:19:25 +0900

    [Silver III] Title: N과 M (3), Time: 432 ms, Memory: 68164 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        sb = new StringBuilder();
+
+        func(0);
+        System.out.print(sb);
+    }
+
+    private static void func(int a) { // 현재 a개까지 수를 택함
+        if(a == m) { // m개를 모두 택하면
+            for (int i = 0; i < m; i++) { // 출력
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) { // 1부터 n까지 수에 대해
+            arr[a] = i; // a번째 수를 i로 채움
+            func(a+1); // 다음칸 채우기
+        }
+    }
+}
