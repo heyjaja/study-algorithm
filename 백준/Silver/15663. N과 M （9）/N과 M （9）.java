216fd91fc1886d80ca0a2054e8dc2b8979140a3c
jaja <89507047+heyjaja@users.noreply.github.com>
2025-08-11 21:43:15 +0900

    [Silver II] Title: N과 M (9), Time: 172 ms, Memory: 21288 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] inputs;
+    static int[] arr;
+    static boolean[] visits;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        inputs = new int[n];
+        arr = new int[n];
+        visits = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            inputs[i] = Integer.parseInt(st.nextToken());
+        }
+
+        sb = new StringBuilder();
+
+        Arrays.sort(inputs);
+        func(0);
+        System.out.print(sb);
+    }
+
+    public static void func(int a) {
+        if(a == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=0; i<n; i++) {
+            if(!visits[i]) {
+                // i가 1이 아닐 때, 이전 수와 지금 수가 같을 때(정렬이 된 상태라 같은 숫자가 붙어있음), 같은 수를 한번만 방문
+                if(i > 0 && inputs[i-1] == inputs[i] && !visits[i-1]) {
+                    continue;
+                }
+                arr[a] = inputs[i];
+                visits[i] = true;
+                func(a + 1);
+                visits[i] = false;
+            }
+        }
+
+    }
+}
